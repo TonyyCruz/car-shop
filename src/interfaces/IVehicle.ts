77eaeedc978fb51b6ea15cf47a205d0ae692fa0c
Anthony Cruz <tony_esqueloto@hotmail.com>
2022-10-21 17:26:23 -0300

    feat: requisito 2 - adiciona interface "IVehicle" com validacao feita com o zod

--- src/interfaces/IVehicle.ts
+++ src/interfaces/IVehicle.ts
@@ -1,5 +1,21 @@
 import { z } from 'zod';
 
-export default interface IVehicle<T> {
-  model: 
-}
+const vehicleZodSchema = z.object({
+  model: z.string()
+    .length(3, { message: 'IVehicle' }),
+  year: z.number()
+    .int()
+    .positive()
+    .min(1900)
+    .max(2022),
+  color: z.string()
+    .length(3),
+  status: z.boolean()
+    .optional(),
+  buyValue: z.number()
+    .int(),
+});
+
+export type IVehicle = z.infer<typeof vehicleZodSchema>;
+
+export default vehicleZodSchema;
